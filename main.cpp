40e80a288327940ab9504cba0af45e5a8e0a93bb
jhorel <jhorel.revilla@ucsp.edu.pe>
2018-06-27 23:13:26 -0500

    27.6.18

---
+++ main.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+using namespace std;
+
+class CRender{
+    public:
+        void mRenderizar(const char* obj){cout<<"renderizando "<<obj<<endl;};
+};
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+class Pareja
+{
+    double a, b;
+
+public:
+    double getA();
+    double getB();
+    void   setA(double n);
+    void   setB(double n);
+};
+
+double Pareja::getA() { return a; }
+double Pareja::getB() { return b; }
+void Pareja::setA(double n) { a = n; }
+void Pareja::setB(double n) { b = n; }
+
+class Suma::Public Pareja{
+    double resultado;
+public:
+    double calcular();
+};
+double Suma::calcular() { return p.getA() + p.getB(); }
+//////////////////////////////////////////////////////
+//////////////////////////////////////////////////////
+class base {
+public:
+  virtual void quien() {cout << "Base" << endl;} // especificar una clase virtual
+};
+
+class primera_d : public base {
+public:
+  void quien() {cout << "Primera derivacion" << endl;}
+};
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+class figura {
+protected:
+ double x, y;
+public:
+  void set_dim(double i, double j=0) {
+   x = i;
+   y = j;
+  }
+  virtual void mostrar_area() = 0; // pura
+};
+
+class triangulo : public figura {
+
+public:
+  void mostrar_area() {
+    cout << "Triangulo con alto ";
+    cout << x << "  y base " << y;
+    cout << " tiene un area de ";
+    cout << x * 0.5 * y << ".\n";
+  }
+};
+
+
+int main()
+{
+    CRender render1,render2;
+    render1.mRenderizar("triangulo");
+    render2.mRenderizar("cuadrado");
+    return 0;
+}
